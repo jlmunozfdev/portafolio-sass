dd9e6915700841b3cf8448537f767e4f7dd14942
Jorge Luis Muñoz Fernandez <jlmunozf@outlook.com>
2023-04-11 10:44:06 -0500

    Add function download cv

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,3 +3,22 @@ const themeToggle = document.querySelector('#toggle');
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark-mode');
 });
+
+/* Button  Download CV */
+function descargarPDF() {
+	// Ruta del archivo PDF en la carpeta del proyecto
+	var rutaPDF = "/assets/cv/cv_jlmunozfdev.pdf";
+  
+	// Crear un enlace de descarga
+	var enlaceDescarga = document.createElement("a");
+  
+	// Asignar el archivo PDF que se descargará
+	enlaceDescarga.href = rutaPDF;
+	enlaceDescarga.download = "mi_cv.pdf";
+  
+	// Simular un clic en el enlace de descarga
+	enlaceDescarga.click();
+  }
+  
+  // Asignar la función de descarga al botón
+  document.getElementById("descargar").addEventListener("click", descargarPDF);
