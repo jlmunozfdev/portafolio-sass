3716ff6d1ee137574828e09f4418c3bb342afff6
Jorge Luis Muñoz Fernandez <jlmunozf@outlook.com>
2023-04-12 12:36:23 -0500

    Add Menu responsive

--- assets/js/app.js
+++ assets/js/app.js
@@ -25,3 +25,14 @@ function descargarPDF() {
 
 
 /* Hamburguer */
+function hamburguer() {
+    document.getElementsByClassName('menu__dropdown')[0].classList.toggle('down');
+    document.getElementsByClassName('arrow')[0].classList.toggle('gone');
+    if (document.getElementsByClassName('dropdown')[0].classList.contains('down')) {
+      setTimeout(function() {
+        document.getElementsByClassName('dropdown')[0].style.overflow = 'visible'
+      }, 500)
+    } else {
+      document.getElementsByClassName('dropdown')[0].style.overflow = 'hidden'
+    }
+  }
